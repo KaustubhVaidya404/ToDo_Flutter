99fb8965a67b14bbdaa44b266d7db9cd37b0ea7c
Kaustubh <pro.vaidya4413@gmail.com>
2023-08-03 22:52:49 +0530

    Initial commit

--- lib/widget/todolist.dart
+++ lib/widget/todolist.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:todo/color/usecolor.dart';
+//import 'package:todo/color/usecolor.dart';
 
 class TodoList extends StatelessWidget {
   const TodoList({super.key});
